cc4ffc1a201dfcb06e11278037d0dcd3724eac33
jellybean <qi777xj@gmail.com>
2020-07-26 08:28:46 +0000

    removed xssstate

--- dwm/files/.ccls-cache/@var@db@repos@jellybean@dwm@files/config.h
+++ dwm/files/.ccls-cache/@var@db@repos@jellybean@dwm@files/config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx	    = 15;	    /* gaps between windows */
+static const unsigned int gappx	    = 15;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -26,8 +26,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Mpv",      NULL,       NULL,	      0,            True,        -1 },
-	{ "Feh",      NULL,       NULL,       0,            True,        -1 },
+	{ "Mpv",      NULL,	  NULL,	      0,         True,        -1 },
 };
 
 /* layout(s) */
@@ -77,16 +76,16 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,		        XK_f,	   togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,		XK_f,	   togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,			            XK_minus,  setgaps,	   {.i = -1 } },
-	{ MODKEY,			            XK_equal,  setgaps,	   {.i = 0  } },
-	{ MODKEY|ShiftMask,		        XK_equal,  setgaps,	   {.i = -1 } },
+	{ MODKEY,			XK_minus,  setgaps,	   {.i = -1 } },
+	{ MODKEY,			XK_equal,  setgaps,	   {.i = 0  } },
+	{ MODKEY|ShiftMask,		XK_equal,  setgaps,	   {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
