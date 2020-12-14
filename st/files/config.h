3763c4136b13c230cdb397c2dc931f4d0024f0a3
qi777xj <qi777xj@gmail.com>
2020-12-14 16:52:53 +0900

    Revert "changed default column size"
    This reverts commit 39970773967133ff7171fac55ff69eebfd7c79b9.

--- st/files/config.h
+++ st/files/config.h
@@ -145,7 +145,7 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 100;
+static unsigned int cols = 80;
 static unsigned int rows = 24;
 
 /*
