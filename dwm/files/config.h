eb38cb2755309ddafe90c2c714e407e3aa558c15
qi777xj <qi777xj@gmail.com>
2021-12-07 23:34:46 +0000

    returned to st

--- dwm/files/config.h
+++ dwm/files/config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_white, "-sb", col_black, "-sf", col_white, NULL };
-static const char *termcmd[]  = { "urxvt", "-pt", "Root", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
