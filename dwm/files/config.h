0c19d86bd7389e2270a0adf0a3d07695a6e8d645
qi777xj <qi777xj@gmail.com>
2023-03-29 10:20:49 +0900

    added systray patch

--- dwm/files/config.h
+++ dwm/files/config.h
@@ -4,6 +4,11 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx	    = 15;	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "MesloLGS Nerd Font:size=11" };
@@ -25,9 +30,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "mpv",      NULL,	      NULL,	      0,            True,        -1 },
-	{ "feh",      NULL,       NULL,       0,            True,        -1 },
+	/* class              instance    title       tags mask     isfloating   monitor */
+	{ "mpv",              NULL,	      NULL,	      0,            True,        -1 },
+	{ "nsxiv",            NULL,       NULL,       0,            True,        -1 },
+	{ "kakaotalk.exe",    NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
@@ -110,8 +116,6 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
