4ab37753421d578daffd4c262c02e8fb78c59470
qi777xj <qi777xj@gmail.com>
2025-09-22 01:53:12 +0900

    add fallback fonts

--- dwm/files/config.h
+++ dwm/files/config.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MesloLGS Nerd Font:size=11" };
+static const char *fonts[]          = { "MesloLGS Nerd Font:size=11", "D2Coding:size=11", "KoPubWorld돋움체:size=11" };
 static const char dmenufont[]       = "MesloLGS Nerd Font:size=11";
 static const char col_black[]       = "#000000";
 static const char col_white[]       = "#ffffff";
